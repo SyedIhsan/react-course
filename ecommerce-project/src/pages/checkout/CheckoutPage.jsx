863533f1ddad009a69fad3eac73e68d3771a3755
SyedIhsan <syedihsan1998@gmail.com>
2026-03-10 09:08:42 +0800

    Generate the HTML for the delivery options

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,8 +1,20 @@
+import axios from 'axios'
+import dayjs from 'dayjs'
+import { useState, useEffect } from 'react'
 import CheckoutHeader from './CheckoutHeader'
 import formatMoney from '../../utils/money'
 import './CheckoutPage.css'
 
 function CheckoutPage({ cart }) {
+	const [ deliveryOptions, setDeliveryOptions ] = useState([]);
+
+	useEffect(() => {
+		axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+			.then((response) => {
+				setDeliveryOptions(response.data);
+			});
+	}, []);
+
 	return (
 		<>
 			<link rel="icon" type="image/svg+xml" href="https://supersimple.dev/images/cart-favicon.png" />
@@ -15,11 +27,15 @@ function CheckoutPage({ cart }) {
 
 				<div className="checkout-grid">
 					<div className="order-summary">
-						{cart.map((cartItem) => {
+						{deliveryOptions.length > 0 && cart.map((cartItem) => {
+							const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
+								return deliveryOption.id === cartItem.deliveryOptionId;
+							});
+
 							return (
 								<div key={cartItem.productId} className="cart-item-container">
 									<div className="delivery-date">
-										Delivery date: Tuesday, June 21
+										Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
 									</div>
 
 									<div className="cart-item-details-grid">
@@ -50,50 +66,31 @@ function CheckoutPage({ cart }) {
 											<div className="delivery-options-title">
 												Choose a delivery option:
 											</div>
-											<div className="delivery-option">
-												<input type="radio" checked
-													className="delivery-option-input"
-													name="delivery-option-1" />
-												<div>
-													<div className="delivery-option-date">
-														Tuesday, June 21
+											{deliveryOptions.map((deliveryOption) => {
+												return (
+													<div key={deliveryOption.id} className="delivery-option">
+														<input type="radio"
+															checked={deliveryOption.id === cartItem.deliveryOptionId}
+															className="delivery-option-input"
+															name={`delivery-option-${cartItem.productId}`} />
+														<div>
+															<div className="delivery-option-date">
+																{dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+															</div>
+															<div className="delivery-option-price">
+																{deliveryOption.priceCents > 0 
+																	? `${formatMoney(deliveryOption.priceCents)} - Shipping` 
+																	: 'FREE Shipping'}
+															</div>
+														</div>
 													</div>
-													<div className="delivery-option-price">
-														FREE Shipping
-													</div>
-												</div>
-											</div>
-											<div className="delivery-option">
-												<input type="radio"
-													className="delivery-option-input"
-													name="delivery-option-1" />
-												<div>
-													<div className="delivery-option-date">
-														Wednesday, June 15
-													</div>
-													<div className="delivery-option-price">
-														$4.99 - Shipping
-													</div>
-												</div>
-											</div>
-											<div className="delivery-option">
-												<input type="radio"
-													className="delivery-option-input"
-													name="delivery-option-1" />
-												<div>
-													<div className="delivery-option-date">
-														Monday, June 13
-													</div>
-													<div className="delivery-option-price">
-														$9.99 - Shipping
-													</div>
-												</div>
-											</div>
+												);
+											})}
 										</div>
 									</div>
 								</div>
 							);
-						})};
+						})}
 					</div>
 
 					<div className="payment-summary">
